b68c99d68594b32a83fdf233d3fc5a097fbbcffd
vaknin <vaknins33@gmail.com>
2019-06-29 21:45:48 +0300

    Updates

---
+++ precache-manifest.e29754184ce629e8ecc7fb140386eece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbea87b41d9c8ebe6b3fc8efc77b6ce0",
+    "url": "/fetch/index.html"
+  },
+  {
+    "revision": "2fe22a9d692e82bd7d46",
+    "url": "/fetch/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "08554bac0e67679c5135",
+    "url": "/fetch/static/css/main.17791047.chunk.css"
+  },
+  {
+    "revision": "2fe22a9d692e82bd7d46",
+    "url": "/fetch/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "08554bac0e67679c5135",
+    "url": "/fetch/static/js/main.261689a3.chunk.js"
+  },
+  {
+    "revision": "3ce6dbaa7fa6459b10e9",
+    "url": "/fetch/static/js/runtime~main.14f83011.js"
+  }
+]);
